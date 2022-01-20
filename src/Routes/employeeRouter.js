78667d79015abec1f8e31068a058d90666b5d5a7
unknown <carolinne.silva888@gmail.com>
2022-01-20 08:53:51 -0300

    feat: add GET route

--- src/Routes/employeeRouter.js
+++ src/Routes/employeeRouter.js
@@ -1,6 +1,8 @@
 const EmployeeController = require('../app/controllers/employeeController');
 
-module.exports = (server, routes, prefix = '/api/v1/employee') => {
-  routes.post('/',  EmployeeController.create);
+module.exports = (server, routes, prefix = '/app/v1') => {
+  routes.post('/employee',  EmployeeController.register)
+  routes.get('/employee',  EmployeeController.getAllEmployes)
+
   server.use(prefix, routes);
 }
--- src/app/controllers/employeeController.js
+++ src/app/controllers/employeeController.js
@@ -1,14 +1,33 @@
 const EmployeeService = require('../service/employeeService.js');
+const employeesSchema = require('../schema/employees');
+
 
 class EmployeeController  {
-  async create(req, res) {
+  async register(req, res) {
     try {
       const result = await EmployeeService.create(req.body);
-      return res.status(201).json(result);
+      return res.status(201).json(result, {} );
     } catch (error) {
       return res.status(500).json( error.message );
     }
   }
+
+  async getAllEmployes(req, res){
+    try {
+   const allEmployees = await employeesSchema.find({})
+
+        return res.status(200).json(allEmployees)
+
+    } catch (error) {
+
+        return res.status(500).json(error.message)
+
+    }
+
 }
+
+}
+
+
 
 module.exports = new EmployeeController();
