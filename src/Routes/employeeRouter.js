7cf107e9e0d008313cdce72a44f1aa3f4282e8ff
unknown <carolinne.silva888@gmail.com>
2022-01-21 18:21:06 -0300

    docs: route file change

--- src/Routes/employeeRouter.js
+++ src/Routes/employeeRouter.js
@@ -1,8 +1,10 @@
 const EmployeeController = require('../app/controllers/employeeController');
 
 module.exports = (server, routes, prefix = '/api/v1/employee') => {
-  routes.post('/',EmployeeController.create);
-
+  routes.post('/',  EmployeeController.create)
+  routes.get('/',  EmployeeController.getAllEmployes)
+  routes.put('/:id',  EmployeeController.updateEmployee)
+  routes.delete('/:id', EmployeeController.deleteEmployee)
   server.use(prefix, routes);
 
 }
