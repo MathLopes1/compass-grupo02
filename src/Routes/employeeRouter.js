687d830b4d5304e7b4b5bb9783b58cacd2955714
Nana <tsampaio21@gmail.com>
2022-01-21 12:13:31 -0300

    fix:fix some errors on post route

--- src/Routes/employeeRouter.js
+++ src/Routes/employeeRouter.js
@@ -2,7 +2,7 @@ const EmployeeController = require('../app/controllers/employeeController');
 
 
 module.exports = (server, routes, prefix = '/app/v1/employee') => {
-  routes.post('/',  EmployeeController.register)
+  routes.post('/',  EmployeeController.create)
   routes.get('/',  EmployeeController.getAllEmployes)
   routes.put('/:id',  EmployeeController.updateEmployee)
   routes.delete('/:id', EmployeeController.deleteEmployee)
