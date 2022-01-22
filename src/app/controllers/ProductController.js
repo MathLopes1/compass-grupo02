a0f01747dea1c80f3b302c1fae17403fb87607ac
Edson Junior <edsonjn2013@gmail.com>
2022-01-22 00:22:10 -0300

    feat: Create query parameters name
    criação de get parameters do campo name via url.

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -6,8 +6,21 @@ class ProductController {
   async find(req,res){
 
     try {
-        const allProducts = await ProductService.find({})
+        const name = req.query.name
+        //const max_price = req.query.max_price
+        //const min_price = req.query.min_price
+        
+        if(name){
+          const queryProducts = await ProductService.find({name: {$regex: name}})
+          return res.status(201).json(queryProducts)
+
+        }else{
+
+        const allProducts = await ProductService.find()
         return res.status(201).json(allProducts)
+
+      }
+        
     } catch (error) {
         return res.status(500).json(error.message)
 }
