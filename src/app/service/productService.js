414183dff14e0d360a15655b1f1a298d305b5732
mylena@usto.re <mylena@usto.re>
2022-01-22 19:05:12 -0300

    feat: adding conditions in get

--- src/app/service/productService.js
+++ src/app/service/productService.js
@@ -8,7 +8,7 @@ class ProductService {
       const employe = await employeesSchema.findOne({ employee_id: payload.employee_id, office: "gerente", situation: "activate" });
 
       if (employe == null) {
-        throw "Funcionário não pode cadastrar produto";
+        throw "Employee cannot register product";
       }
 
       const result = await ProductRepository.create(payload);
