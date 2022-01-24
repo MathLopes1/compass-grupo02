019306959c03f63c770fea984a91ba2c018b817e
mylena@usto.re <mylena@usto.re>
2022-01-23 23:19:19 -0300

    ref: ref app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = require('./routes');
-const cors = require('cors')
+const cors = require('cors');
 require('./infra/database/mongo/index');
 
 class App {
